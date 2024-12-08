a3f3383b71bcfbe6aa799aef2fc4e265410bad24
dbwoflaqnzo3 <141646829+dbwoflaqnzo3@users.noreply.github.com>
2024-12-08 05:58:18 +0000

    feat : basic edition

---
+++ my-app/src/app/components/LoginForm.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+const LoginForm = () => {
+    return (
+        <h1>테스트용 해시</h1>
+    );
+  };
+
+export default LoginForm;
---
+++ my-app/src/app/page.js
@@ -0,0 +1,5 @@
+
+
+export default function Home() {
+  return <h1>여기가 집이긴 한데 쓸모가 없다</h1>;
+}
